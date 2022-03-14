602a181480160a187fa899bf6cf878c2a3cd5450
tonlirise <tonlirise@gmail.com>
2022-03-14 23:43:29 +0700

    Добавлен диалог выбора времени

--- app/src/main/java/com/example/simplealarm/MainActivity.kt
+++ app/src/main/java/com/example/simplealarm/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.simplealarm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSetTime = findViewById<Button>(R.id.btnSetTime)
+        btnSetTime.setOnClickListener{
+            val timePicker = MaterialTimePicker.Builder()
+                .setTitleText(getString(R.string.select_time_text))
+                .setTimeFormat(TimeFormat.CLOCK_24H)
+                .setHour(12)
+                .setMinute(0)
+                .build()
+            timePicker.show(supportFragmentManager, PICKER_TAG)
+        }
+    }
+
+    companion object{
+        const val PICKER_TAG = "show_picker"
     }
 }
