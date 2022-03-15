2e037427e57fa447e8c7f927c83c7172bf1958b2
tonlirise <tonlirise@gmail.com>
2022-03-15 16:01:45 +0700

    Добавлен звуковой сигнал

--- app/src/main/java/com/example/simplealarm/AlarmActivity.kt
+++ app/src/main/java/com/example/simplealarm/AlarmActivity.kt
@@ -1,11 +1,27 @@
 package com.example.simplealarm
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.Ringtone
+import android.media.RingtoneManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class AlarmActivity : AppCompatActivity() {
+    var rington : Ringtone? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarm)
+
+        var ringtonUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
+        if (ringtonUri == null){
+            ringtonUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE)
+        }
+        rington = RingtoneManager.getRingtone(this,ringtonUri)
+        rington?.play()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        rington?.stop()
     }
 }
